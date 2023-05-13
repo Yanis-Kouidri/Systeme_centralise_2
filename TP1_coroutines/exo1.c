aa8aca2f6637853b7ff150c8b02c29ae990ff012
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-05-13 17:12:42 +0200

    Debut TP1

--- TP1_coroutines/exo1.c
+++ TP1_coroutines/exo1.c
@@ -1,3 +1,10 @@
+/* 1) Écrire un programme qui affiche 1,2,3,4,5 avec 4 coroutines :
+    - une procédure affiche 1 et 3
+    - une procédure affiche 2 et 5
+    - une procédure affiche 4
+    - le programme conclut avec un message indiquant la fin.
+   (squelette disponible dans exo1.c)
+*/
 
 /* Doit afficher: "1 2 3 4 5"
  *  et terminer par "c'est fini."
@@ -7,25 +14,37 @@
 #include <stdlib.h>
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
+coroutine_t c1, c2, c3, cm;
+
 void code1 (void *unused)
 {
     printf("1\n");
+    cor_transferer(c1, c2);
     printf("3\n");
+    cor_transferer(c1, c3);
 }
 
 void code2 (void *unused)
 {
     printf("2\n");
+    cor_transferer(c2, c1);
     printf("5\n");
+    cor_transferer(c2, cm);
 }
 
 void code3 (void *unused)
 {
     printf("4\n");
+    cor_transferer(c3, c2);
 }
 
 int main()
 {
+    c1 = cor_creer("C1", code1, NULL);
+    c2 = cor_creer("C2", code2, NULL);
+    c3 = cor_creer("C3", code3, NULL);
+    cm = cor_creer("CM", NULL, NULL);
+    cor_transferer(cm, c1);
     printf ("C'est fini.\n");
     return 0;
 }
--- TP1_coroutines/exo2.c
+++ TP1_coroutines/exo2.c
@@ -6,7 +6,16 @@
  *
  * Résultat attendu: doit afficher 3 fois chaque nombre 1 à N
  * dans le désordre et terminer par "fini."
+ *
+ *
+ * 2) On souhaite réaliser un distributeur de travaux. Chaque travail (supposé
+ *    long) est décomposé en trois parties. Entre chaque partie, le travail
+ *    doit redonner la main au distributeur pour donner une chance aux autres
+ *    travaux de progresser.
+ *    (squelette disponible dans exo2.c)
+ *
  */
+
 #include "coroutines.h"
 #include <stdio.h>
 #include <unistd.h>             /* for getpid */
@@ -15,6 +24,8 @@
 #define N 10
 
 /* XXXX Une coroutine par travail, plus le distributeur et le prog principal */
+coroutine_t tab_co[N];
+coroutine_t dist, cm;
 
 /* Vrai si un travail est terminé. */
 static int fini[N];
@@ -25,11 +36,16 @@ void travail (void *pi)
     i = *(int *)pi;
     printf ("Travail %d (step 1)\n", i);
     /* XXXX rendre la main au distributeur */
+    cor_transferer(tab_co[i], dist);
+
     printf ("Travail %d (step 2)\n", i);
     /* XXXX rendre la main au distributeur */
+    cor_transferer(tab_co[i], dist);
+
     printf ("Travail %d (step 3)\n", i);
     fini[i] = 1; /* enregistre que le travail est fini. */
     /* XXXX rendre la main au distributeur */
+    cor_transferer(tab_co[i], dist);
 }
 
 /* Vrai si tous les travaux sont terminés. */
@@ -52,8 +68,10 @@ void distribuer (void *unused)
             n = random() % N;
         } while (fini[n]);
         /* XXXX Donne la main à ce travail. */
+        cor_transferer(dist, tab_co[n]);
     }
     /* XXXX Quand tous les travaux sont finis, rend la main au prog. princ. */
+    cor_transferer(dist, cm);
 }
 
 
@@ -62,14 +80,22 @@ int main()
     int i;
     srandom (getpid()); /* init du générateur de nombres aléatoires */
     /* XXXX Crée le distributeur de travail. */
+    dist = cor_creer("Cdist", distribuer, NULL);
+    cm = cor_creer("CM", NULL, NULL);
+    
+
     /* XXXX Crée les travaux */
     for (i = 0; i < N; i++) {
         int *m = malloc (sizeof (int));
         *m = i;
         fini[i] = 0;
+        tab_co[i] = cor_creer("C", travail, m );
     }
     /* XXXX Prog principal. */
+    cor_transferer(cm, dist);
+
     /* XXXX Donne la main au distributeur. */
     printf ("Fini.\n");
     return 0;
 }
+
