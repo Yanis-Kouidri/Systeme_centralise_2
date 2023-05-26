fc9014d89e7d87cc89147576a5d3efa31c1ae1ca
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-05-26 17:27:15 +0200

    removing some comment for debugging

--- TP2_processus/exo1.c
+++ TP2_processus/exo1.c
@@ -61,34 +61,3 @@ int main ()
 
     return 0;
 }
-
-
-/*
-int compteur;
-
-void code1(void *unused) {
-    for (int i = 0; i < 1000; i++) {
-        printf("P1 : %d\n", i);
-        compteur++;
-        proc_continuer(p2);
-        proc_suspendre();
-    }
-}
-
-void code2(void *unused) {
-    proc_suspendre();
-    while (compteur < 10) {
-        printf("P2 : %d\n", compteur);
-        proc_continuer(p1);
-        proc_suspendre();
-    }
-    proc_continuer(pmain);  redonne la main à main 
-}
-
-int main() {
-    proc_init();
-    p1 = proc_activer("P1", code1, NULL);
-    proc_suspendre();
-    return 0; terminaison du système 
-}
-*/
